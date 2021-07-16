22d7f90d8cd80ccc8280e945e410971fa863aa16
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2021-07-16 17:41:10 +0200

    Initial commit

---
+++ src/main/java/org/nextprot/vep/VepApplication.java
@@ -0,0 +1,13 @@
+package org.nextprot.vep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nextprot/vep/VepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nextprot.vep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
