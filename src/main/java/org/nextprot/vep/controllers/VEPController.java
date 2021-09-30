9373aabed815c739ae09e9ea7e7f4717cecbfd17
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2021-09-30 17:30:28 +0200

    Added more comments and clean up

--- src/main/java/org/nextprot/vep/controllers/VEPController.java
+++ src/main/java/org/nextprot/vep/controllers/VEPController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
--- src/main/java/org/nextprot/vep/services/AminoAcidService.java
+++ src/main/java/org/nextprot/vep/services/AminoAcidService.java
@@ -1,5 +1,8 @@
 package org.nextprot.vep.services;
 
+/**
+ * Provide amino acid code convertion
+ */
 public interface AminoAcidService {
 
     String getThreeLetterCode(String aminoAcidOneLetterCode) throws Exception;
