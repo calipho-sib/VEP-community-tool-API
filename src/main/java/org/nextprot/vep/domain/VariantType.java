2f30587963301a56600fc4400ae90155585bdece
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2021-10-27 15:40:22 +0200

    Add HGVS variant types

---
+++ src/main/java/org/nextprot/vep/domain/VariantType.java
@@ -0,0 +1,13 @@
+package org.nextprot.vep.domain;
+
+public enum VariantType {
+    SUBSTITUTION,
+    DELETION,
+    DUPLICATION,
+    INSERTION,
+    DELETION_INSERTION,
+    ALLELS,
+    REPEATEDSEQUENCES,
+    FRAMESHIFT,
+    EXTENSION
+}
